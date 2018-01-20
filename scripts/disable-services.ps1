60169b4f6b5746b3a1c7537d3976077ce98d913b
Marco Hinz <hinzigers@users.noreply.github.com>
2018-01-20 17:28:23 +0100

    add services

--- scripts/disable-services.ps1
+++ scripts/disable-services.ps1
@@ -5,7 +5,7 @@
 $services = @(
     "diagnosticshub.standardcollector.service" # Microsoft (R) Diagnostics Hub Standard Collector Service
     "DiagTrack"                                # Diagnostics Tracking Service
-    "dmwappushservice"                         # WAP Push Message Routing Service (see known issues)
+    "dmwappushservice"                         # WAP Push Message Routing Service
     "HomeGroupListener"                        # HomeGroup Listener
     "HomeGroupProvider"                        # HomeGroup Provider
     "lfsvc"                                    # Geolocation Service
@@ -13,17 +13,80 @@ $services = @(
     "NetTcpPortSharing"                        # Net.Tcp Port Sharing Service
     "RemoteAccess"                             # Routing and Remote Access
     "RemoteRegistry"                           # Remote Registry
-    "SharedAccess"                             # Internet Connection Sharing (ICS)
+    #"SharedAccess"                             # Internet Connection Sharing (ICS)
     "TrkWks"                                   # Distributed Link Tracking Client
     "WbioSrvc"                                 # Windows Biometric Service
     #"WlanSvc"                                 # WLAN AutoConfig
-    "WMPNetworkSvc"                            # Windows Media Player Network Sharing Service
+    #"WMPNetworkSvc"                            # Windows Media Player Network Sharing Service | ausklammern wenn der Player bereits deinstalliert wurde
     "wscsvc"                                   # Windows Security Center Service
-    #"WSearch"                                 # Windows Search
-    "XblAuthManager"                           # Xbox Live Auth Manager
-    "XblGameSave"                              # Xbox Live Game Save Service
-    "XboxNetApiSvc"                            # Xbox Live Networking Service
-
+    "WSearch"                                  # Windows Search
+    "XblAuthManager"                           # Xbox Live Auth Manager | ausklammern wenn der Player bereits deinstalliert wurde
+    "XblGameSave"                              # Xbox Live Game Save Service | ausklammern wenn der Player bereits deinstalliert wurde
+    "XboxNetApiSvc"                            # Xbox Live Networking Service | ausklammern wenn der Player bereits deinstalliert wurde
+    "XboxGipSvc"                               # Xbox Accessory Management Service | ausklammern wenn der Player bereits deinstalliert wurde
+    #"xbgm"                                     # Xbox Game Monitoring | ZUGRIFF VERWEIGERT!!! Dienst löschen!!
+    "SEMgrSvc"                                 # Zahlungs- und NFC/SE-Manager
+    #"MessagingService_3cdefe"                  # MessagingService_3cdefe
+    #"UnistoreSvc_3cdefe"                       # Benutzerdatenspeicher _3cdefe
+    #"UserDataSvc_3cdefe"                       # Benutzerdatenzugriff_3cdefe
+    #"CDPUserSvc_3cdefe"                        # Benutzerdienst für die Plattform für verbundene Geräte_3cdefe
+    "DiagTrack"                                # Benutzererfahrung und Telemetrie im verbundenen Modus
+    #"UserDataSvc_23721"
+    #"UnistoreSvc_23721"
+	"WpnService"                                # Windows-Pushbenachrichtigungssystemdienst
+	
+    
+    "WerSvc"
+    "WebClient"
+    "upnphost"
+    "DoSvc"
+    "SSDPSRV"
+    "PcaSvc"
+    "PolicyAgent"
+    "ALG"
+    "Fax"
+    #"WcsPlugInService"
+    "Wecsvc"
+    #"MpsSvc"                                     # Windows Firewall
+    "WbioSrvc"                                   # Windows-Biometriedienst
+    "icssvc"                                     # Windows-Dienst fÃŒr mobile Hotspots
+    "NcaSvc"                                     # NetzwerkkonnektivitÃ€ts-Assistent
+    "iphlpsvc"                                   # IP-Hilfsdienst
+    "WinHttpAutoProxySvc"                        # WinHTTP-Web Proxy Auto-Discovery-Dienst
+    "WalletService"                              # Von Clients der Funktion 'Brieftasche' verwendete Hostobjekte
+	#"UsoSvc"                                     # Update Orchestrator Service | wenn Dienst deaktiviert wird, lassen sich in den Einstellungen Update und Sicherheit nicht mehr oeffnen
+	"PhoneSvc"                                   # Verwaltet den Telefoniestatus des Geräts
+	"TapiSrv"                                    # Tel-API-Support (TAPI)
+    #"EntAppSvc"                                  # Verwaltungsdienst für Unternehmens-Apps | ZUGRIFF VERWEIGERT!!! Dienst löschen!!
+	"spectrum"                                   # Windows Perception Service (Ermöglicht die räumliche Wahrnehmung, räumliche Eingaben und holografisches Rendering.)
+	"wisvc"                                      # Windows-Insider-Dienst
+	"FrameServer"                                # Windows-Kamera-FrameServer
+	"LicenseManager"                             # Windows-Lizenz-Manager-Dienst
+	"WinRM"                                      # Windows-Remoteverwaltung (WS-Verwaltung)
+	"wcncsvc"                                    # Windows-Sofortverbindung - Konfigurationsregistrierungsstelle | Implementierung des WPS (Wireless Protected Setup)-Protokoll
+	"RetailDemo"                                 # Dienst für Einzelhandelsdemos
+	"SmsRouter"                                  # Microsoft Windows SMS-Routerdienst
+	"PNRPsvc"                                    # Peer Name Resolution-Protokoll
+	"p2psvc"                                     # Peernetzwerk-Gruppenzuordnung
+	"p2pimsvc"                                   # Peernetzwerkidentitäts-Manager
+	"PNRPAutoReg"                                # PNRP-Computernamenveröffentlichungs-Dienst
+	"HvHost"                                     # HV-Hostdienst
+	"vmicvmsession"                              # Hyper-V PowerShell Direct-Dienst
+	"vmickvpexchange"                            # Hyper-V-Datenaustauschdienst
+	"vmictimesync"                               # Hyper-V-Dienst für Zeitsynchronisierung
+	"vmicshutdown"                               # Hyper-V-Dienst zum Herunterfahren des Gasts
+	"vmicguestinterface"                         # Hyper-V-Gastdienstschnittstelle
+	"vmicrdv"                                    # Hyper-V-Remotedesktopvirtualisierungsdienst
+	"vmicheartbeat"                              # Hyper-V-Taktdienst
+	"vmicvss"                                    # Hyper-V-Volumeschattenkopie-Anforderer
+	"AJRouter"                                   # AllJoyn-Routerdienst
+	#"wlidsvc"                                   # Anmelde-Assistent für Microsoft-Konten | gilt auch für UNC Mapping
+	#"embeddedmode"
+    "AppReadiness"
+    #"Schedule"
+    "DevQueryBroker"
+    #"SystemEventsBroker"
+    
     # Services which cannot be disabled
     #"WdNisSvc"
 )
